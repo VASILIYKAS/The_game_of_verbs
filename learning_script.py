f1cca3eb2ddb50f1131fd7225cfd3a8d9966df9e
VASILIYKAS <yoyoynv@gmail.com>
2025-06-18 18:02:46 +0500

    add DialogFlow learning script.

---
+++ learning_script.py
@@ -0,0 +1,39 @@
+import json
+from google.cloud import dialogflow
+
+
+def create_intent(project_id):
+    with open('questions.json', 'r', encoding='utf-8') as my_file:
+        question_json = json.load(my_file)
+
+    intents_client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+
+    for intent_name, intent_data in question_json.items():
+        training_phrases = []
+
+        for phrase in intent_data['questions']:
+            part = dialogflow.Intent.TrainingPhrase.Part(text=phrase)
+            training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+            training_phrases.append(training_phrase)
+
+        message_texts = [intent_data["answer"]]
+        text = dialogflow.Intent.Message.Text(text=message_texts)
+        message = dialogflow.Intent.Message(text=text)
+
+        intent = dialogflow.Intent(
+            display_name=intent_name,
+            training_phrases=training_phrases,
+            messages=[message]
+        )
+
+        response = intents_client.create_intent(
+            request={'parent': parent, 'intent': intent}
+        )
+        print(f'Интент "{intent_name}" успешно создан!')
+
+
+if __name__ == "__main__":
+    create_intent(
+        project_id='the-game-of-verbs-iefy'
+    )
