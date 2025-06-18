80761dd7a3021f1262d784137e47c5ff11586213
VASILIYKAS <yoyoynv@gmail.com>
2025-06-18 17:13:39 +0500

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -12,13 +12,36 @@ from telegram.ext import (
 )
 from telegram import Update
 
+from google.cloud import dialogflow
+
+
+def detect_intent_texts(user_id, texts):
+    project_id = 'the-game-of-verbs-iefy'
+    language_code = 'ru-RU'
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, user_id)
+
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('/start')
+    update.message.reply_text('Привет! Я бот с интеграцией Dialogflow. Напиши мне что-нибудь.')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def handle_message(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    user_text = update.message.text
+
+    bot_response = detect_intent_texts(user_id, user_text)
+    update.message.reply_text(bot_response)
 
 
 def main():
@@ -37,7 +60,7 @@ def main():
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(MessageHandler(
         Filters.text & ~Filters.command,
-        echo
+        handle_message
         )
     )
 
